664bf48388548db7ed7cf109d4598e4cf72b8749
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2016-06-30 16:42:41 +1000

    Added cleanup handler

---
+++ docker/cleaner/index.js
@@ -0,0 +1,72 @@
+const async = require('async')
+const redis = require('redis')
+const spawn = require('child_process').spawn
+
+const redisHost = '172.17.0.3'
+const dockerHost = 'tcp://172.17.0.1:2375'
+
+const exec = (exe, args, callback) => {
+  const cmd = spawn(exe, args)
+  let output = ''
+  cmd.stdout.on('data', (data) => {
+    output += data
+  })
+
+  cmd.stderr.on('data', (data) => {
+
+  })
+
+  cmd.on('close', (code) => {
+    callback(null, output)
+  })
+}
+
+const listFromRedis = (callback) => {
+  const client = redis.createClient(6379, redisHost)
+  client.keys('*', function (err, res) {
+    const ret = res
+      .filter(ent => ent.length == 64)
+      .map(ent => ent.substr(0, 12))
+
+    callback(null, ret)
+
+    client.quit()
+  })
+}
+
+const listFromDocker = (callback) => {
+  const cmd = exec('docker', ['-H', dockerHost, 'ps', '-f', 'label=corefn=true', '--format', '{{.ID}}'], (err, res) => {
+    const ret = res
+      .split('\n')
+      .filter((ent) => ent && ent.length > 0)
+
+    callback(null, ret)
+  })
+}
+
+const run = () => {
+  async.parallel([
+    listFromRedis,
+    listFromDocker
+  ], (err, results) => {
+    if (err) {
+      return console.error(err);
+    }
+    const redis = results[0]
+    const docker = results[1]
+
+    const kill = docker.filter(ent => redis.indexOf(ent) === -1)
+
+    async.each(kill, (ent, callback) => {
+      console.log(`killing ${ent}`)
+      exec('docker', ['-H', dockerHost, 'kill', ent], callback)
+    }, (err, res) => {
+      if (err) {
+        return console.error(err)
+      }
+      console.log(res)
+    })
+  })
+}
+
+setInterval(run, 60000)
